ea132636b8f724ec21ab8e041a9cb428520a9123
gmahes <gregi.mahes@gmail.com>
2023-09-22 10:55:52 +0700

    Tugas 1 (Instalasi CI3 dan mengerjakan contoh1)

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Contoh1 extends CI_Controller
+{
+
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo "Nama saya Gregi Maulana Mahes
+			Saya tinggal di Tegal
+			Hobi saya tidak ada";
+	}
+}
